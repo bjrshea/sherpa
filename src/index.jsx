7e7791d6bd86db788cc59e5c3a212a3ec5827eb4
Brendan Shea <bjrshea@gmail.com>
2018-11-30 16:50:59 -0800

    Add linter to dev environment

--- src/index.jsx
+++ src/index.jsx
@@ -4,18 +4,20 @@ import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
 
 const render = (Component) => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component/>
-    </AppContainer>,
-    document.getElementById('react-app-root')
-  );
+	ReactDOM.render(
+		<AppContainer>
+			<Component/>
+		</AppContainer>,
+		document.getElementById('react-app-root')
+	);
 };
 
 render(App);
 
+/*eslint-disable */
 if (module.hot) {
   module.hot.accept('./components/App', () => {
-    render(App)
+    render(App);
   });
 }
+/*eslint-enable */
