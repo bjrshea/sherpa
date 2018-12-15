96bf39647d2784da1f3379488c59fa09c0539de9
Brendan Shea <bjrshea@gmail.com>
2018-12-14 20:28:48 -0800

    Add UserCreds component and styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import SearchLocation from './SearchLocation';
+import UserCreds from './UserCreds';
 import mountains from './../assets/images/background.svg';
 
 function Hero() {
   return(
   <div>
+    <UserCreds/>
     <div className="logo">
       <h1>Sherpa</h1>
       <h3>Your guide to resorts unknown...</h3>
     </div>
-    <div>
-      <SearchLocation/>
-    </div>
+    <SearchLocation/>
     <div className="background">
       <img className="mountains" src={mountains} alt="mountains"/>
     </div>
---
+++ src/components/UserCreds.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserCreds() {
+  return(
+  <div className="user-creds">
+    <p><span className="sign-up">Sign Up</span> | Sign In</p>
+  </div>
+  );
+}
+
+export default UserCreds;
