da4f99f594793b1a4b248587ee8e25de7401be9d
Brendan Shea <bjrshea@gmail.com>
2018-12-17 09:15:41 -0800

    Add firebase config to project

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,21 @@
+const coordsArray = [];
+console.log(coordsArray);
+
 export function fetchUserInput(inputtedState) {
-  const lngAndLat = [];
   return function (dispatch) {
     return fetch(`https://liftie.info/api/meta`).then(
       response => response.json(),
       error => console.log('An error occurred.', error)
     ).then(function(json) {
       for (let i = 0; i < json.length; i++) {
-        lngAndLat.push(json[i].ll);
+        coordsArray.push(json[i].ll);
       }
-    });
+    })
   };
 };
+
+// export function fetchLocation() {
+//   coordsArray.forEach(function(lngAndLat) {
+//     console.log(lngAndLat);
+//   });
+// }
---
+++ src/constants/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+   apiKey: "AIzaSyDw_qiLXZQF8TqjA5LPRtXzOzEByOq4szo",
+   authDomain: "ski-sherpa.firebaseapp.com",
+   databaseURL: "https://ski-sherpa.firebaseio.com",
+   projectId: "ski-sherpa",
+   storageBucket: "",
+   messagingSenderId: "513912773024"
+ };
+
+ export default firebaseConfig;
